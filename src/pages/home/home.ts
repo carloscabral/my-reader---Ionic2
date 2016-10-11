e833b66a950e83022dd4719f1b421ff2964b2bc8
Carlos Cabral <carlos.gomescabral@gmail.com>
2016-10-11 20:18:53 -0300

    Feching reddit API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  public feeds: Array<string>;
+  private url: string = "https://www.reddit.com/new.json";  
+
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.http.get(this.url).map(res => res.json())
+      .subscribe(data => {
+        this.feeds = data.data.children;
+      }); 
   }
 
 }
