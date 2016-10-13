889fd6bad22244538b12d68cdad7fb4f787f36f6
Carlos Cabral <carlos.gomescabral@gmail.com>
2016-10-13 12:10:01 -0300

    Added Loading component and capturing tap event

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -12,13 +12,28 @@ export class HomePage {
   public feeds: Array<string>;
   private url: string = "https://www.reddit.com/new.json";  
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+
+    this.fetchContent();
+
+  }
+
+  fetchContent ():void {
+    let loading = this.loadingCtrl.create({
+      content: 'Fetching content...'
+    });
+
+    loading.present();
 
     this.http.get(this.url).map(res => res.json())
       .subscribe(data => {
         this.feeds = data.data.children;
-        console.log(this.feeds);
-      }); 
+        loading.dismiss();
+      });  
   }
+
+  itemSelected (feed):void {
+    alert(feed.data.url);
+  }       
 
 }
